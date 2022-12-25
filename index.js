46b3d8b10718564e05e923634de823ee37e5c23f
deekshith03 <deekshith.19cs@kct.ac.in>
2022-12-25 06:40:49 +0530

    added port no as env variable

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 connectDB()
   .then(() => {
